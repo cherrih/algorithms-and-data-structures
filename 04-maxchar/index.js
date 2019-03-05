68fa069f5be239af3e2ecb11130331e821f2840a
cherrih <cherri.hartigan@gmail.com>
2019-03-05 13:24:11 -0800

    Solve maxchar

---
+++ 04-maxchar/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const obj = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+  for (let key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
+
+module.exports = maxChar;
