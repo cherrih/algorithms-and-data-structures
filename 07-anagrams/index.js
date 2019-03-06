1d28626b30c2af3543f6ee132c66f879cae6f579
cherrih <cherri.hartigan@gmail.com>
2019-03-06 12:21:21 -0800

    Complete anagrams solution with helper function

--- 07-anagrams/index.js
+++ 07-anagrams/index.js
@@ -8,17 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  const a = stringA.replace(/[^\w]/g, '').toLowerCase();
-  const b = stringB.replace(/[^\w]/g, '').toLowerCase();
-  if (a.length !== b.length) {
-    return false;
+// function anagrams(stringA, stringB) {
+//   const a = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   const b = stringB.replace(/[^\w]/g, '').toLowerCase();
+//   if (a.length !== b.length) {
+//     return false;
+//   }
+//   const aMap = {};
+//   const bMap = {};
+//   for (let i = 0; i < a.length; i ++) {
+//     aMap[a[i]] = aMap[a[i]] + 1 || 1;
+//     bMap[b[i]] = bMap[b[i]] + 1 || 1;
+//   }
+//   for (let key in aMap) {
+//     if (aMap[key] !== bMap[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+const buildCharMap = (str) => {
+  const charMap = {};
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
   }
-  const aMap = {};
-  const bMap = {};
-  for (let i = 0; i < a.length; i ++) {
-    aMap[a[i]] = aMap[a[i]] + 1 || 1;
-    bMap[b[i]] = bMap[b[i]] + 1 || 1;
+  return charMap;
+}
+
+const anagrams = (stringA, stringB) => {
+  const aMap = buildCharMap(stringA);
+  const bMap = buildCharMap(stringB);
+
+  if (Object.keys(aMap).length !== Object.keys(bMap).length) {
+    return false;
   }
   for (let key in aMap) {
     if (aMap[key] !== bMap[key]) {
@@ -27,5 +50,6 @@ function anagrams(stringA, stringB) {
   }
   return true;
 }
+
 
 module.exports = anagrams;
