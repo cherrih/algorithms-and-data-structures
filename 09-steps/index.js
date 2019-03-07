40e158cfc9d004399b691a719e57ac05556f32e6
cherrih <cherri.hartigan@gmail.com>
2019-03-07 11:55:44 -0800

    Solve steps recursively

--- 09-steps/index.js
+++ 09-steps/index.js
@@ -17,18 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let i = 0; i < n; i ++) {
-    let step = '';
-    for (let j = 0; j < n; j ++) {
-      if (j <= i) {
-        step += '#';
-      } else {
-        step += ' ';
-      }
-    }
-    console.log(step);
+// iterative solution
+
+// function steps(n) {
+//   for (let i = 0; i < n; i ++) {
+//     let step = '';
+//     for (let j = 0; j < n; j ++) {
+//       if (j <= i) {
+//         step += '#';
+//       } else {
+//         step += ' ';
+//       }
+//     }
+//     console.log(step);
+//   }
+// }
+
+// recursive solution
+
+const steps = (n, row = 0, step = '') => {
+  // base case
+  if (n === row) {
+    return;
   }
+  // if hit end of a row
+  if (n === step.length) {
+    console.log(step);
+    return steps(n, row + 1);
+  } 
+  if (step.length <= row) {
+    step += '#';
+  } else {
+    step += ' ';
+  }
+  steps(n, row, step);
 }
 
 module.exports = steps;
