b4a4c145e48f15cc01da13223b0c313a1ee1307e
cherrih <cherri.hartigan@gmail.com>
2019-03-08 11:16:25 -0800

    Complete recursive solution pyramid

--- 10-pyramid/index.js
+++ 10-pyramid/index.js
@@ -14,19 +14,37 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const maxCol = 1 + (n - 1 ) * 2;
-  for (let row = 0; row < n; row ++ ) {
-    let level = '';
-    for (let col = 0; col < maxCol; col ++) {
-      if (col <= n - 1 + row && col >= n - 1 - row) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
-    console.log(level);
+// iterative solution
+
+// function pyramid(n) {
+//   const maxCol = n * 2 - 1;
+//   for (let row = 0; row < n; row ++ ) {
+//     let level = '';
+//     for (let col = 0; col < maxCol; col ++) {
+//       if (col <= n - 1 + row && col >= n - 1 - row) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// recursive solution
+
+const pyramid = (n, row = 0, col = 0, level = '') => {
+  const maxCol = n * 2 - 1;
+  // base case return 
+  if (row === n) {
+    return;
   }
+  if (level.length === maxCol) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const add = col <= n - 1 + row && col >= n - 1 - row ? '#' : ' ';
+  pyramid(n, row, col + 1, level + add);
 }
 
 module.exports = pyramid;
