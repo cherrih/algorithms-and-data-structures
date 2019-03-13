02a9ff7ef545c3fe0fa93d2905d4233b03bb131b
cherrih <cherri.hartigan@gmail.com>
2019-03-13 15:46:19 -0700

    Complete recursive solution fib

--- 13-fib/index.js
+++ 13-fib/index.js
@@ -10,12 +10,27 @@
 
 // iterative solution
 
-function fib(n) {
-  const fibs = [0, 1]
-  for (var i = 2; i <= n; i ++) {
-    fibs.push(fibs[i - 1] + fibs[i - 2]);
+// function fib(n) {
+//   const fibs = [0, 1];
+//   for (let i = 2; i <= n; i ++) {
+//     fibs.push(fibs[i - 1] + fibs[i - 2]);
+//   }
+//   return fibs[n];
+// }
+
+// Runtime complexity is O(n) - linear runtime
+// for every increase in n, we have to calculate one additional number
+
+// recursive solution
+
+const fib = (n, fibs = [0, 1]) => {
+  if (fibs.length === n + 1){
+    return fibs[n];
   }
-  return fibs[n];
+  const a = fibs[fibs.length - 1];
+  const b = fibs[fibs.length - 2];
+  fibs.push(a + b);
+  return fib(n, fibs);
 }
 
 module.exports = fib;
