dffc25a4556573532d9c3989daf4f43c84ba39e8
cherrih <cherri.hartigan@gmail.com>
2019-03-28 15:51:13 -0700

    Implement trigger events solution

--- 26-events/index.js
+++ 26-events/index.js
@@ -9,22 +9,25 @@ class Events {
   }
   // Register an event handler
   on(eventName, callback) {
-    if (this.events.eventName) {
+    if (this.events[eventName]) {
       this.events[eventName].push(callback);
     } else {
-      this.events[eventName] = [eventName];
+      this.events[eventName] = [callback];
     }
   }
 
   // Trigger all callbacks associated
   // with a given eventName
   trigger(eventName) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(callback => callback());
+    }
   }
 
   // Remove all event handlers associated
   // with the given eventName
   off(eventName) {
-    delete this.events.eventName;
+    delete this.events[eventName];
   }
 }
 
