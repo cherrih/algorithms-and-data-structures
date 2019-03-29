762ba7a19e1c045651d1177701c97acdec46820e
cherrih <cherri.hartigan@gmail.com>
2019-03-29 12:36:35 -0700

    Refactor naive solution bubblesort

--- 27-sorting/index.js
+++ 27-sorting/index.js
@@ -2,12 +2,12 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-  for (i = 0; i < arr.length; i ++) {
-    for(j = 0; j < arr.length - i; j ++) {
+  for (let i = 0; i < arr.length; i ++) {
+    for(let j = 0; j < arr.length - i - 1; j ++) {
       if(arr[j] > arr[j + 1]) {
-        const curr = arr[j + 1];
+        const lesser = arr[j + 1];
         arr[j + 1] = arr[j];
-        arr[j] = curr;
+        arr[j] = lesser;
       }
     }
   }
