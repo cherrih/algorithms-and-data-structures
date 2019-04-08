d00370ec0b75221a92cf81995a97236b3c39cfb2
cherrih <cherri.hartigan@gmail.com>
2019-04-08 12:23:29 -0700

    Implement merge sort

--- 27-sorting/index.js
+++ 27-sorting/index.js
@@ -33,9 +33,16 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
+// two sorted arrays and join together in correct order
 function merge(left, right) {
   const results = [];
   while (left.length && right.length) {
