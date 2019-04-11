3102b4af7ba2caeb3bd011efff3ef11010012f1c
cherrih <cherri.hartigan@gmail.com>
2019-04-11 11:05:21 -0700

    Implement isSubsequence recursive solution

---
+++ 02-Colt-Steele/05-Recursion/isSubsequence.js
@@ -0,0 +1,10 @@
+const isSubsequence = (str1, str2) => {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str1[0] === str2[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+  return isSubsequence(str1, str2.slice(1))
+}
+
+console.log(isSubsequence('hello','hello world')) // true
+console.log(isSubsequence('abc','acb')) // false
+console.log(isSubsequence('ahoy','a holiday')) //true
