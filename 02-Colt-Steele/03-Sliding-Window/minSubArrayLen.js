5f34661c74315247c60b63a27ccf961808b6f5a4
cherrih <cherri.hartigan@gmail.com>
2019-04-13 12:03:49 -0700

    Implement minSubArrayLen solution

--- 02-Colt-Steele/03-Sliding-Window/minSubArrayLen.js
+++ 02-Colt-Steele/03-Sliding-Window/minSubArrayLen.js
@@ -6,17 +6,17 @@ function minSubArrayLen(arr, n) {
   let right = 1;
   let minLength = Infinity;
   let sum = arr[0];
-  // keep track of length of the array 
-  // i minus count is index of first el of subarray
   while(right <= arr.length){
     if (sum < n) {
       sum += arr[right++]
     } else {
       if (minLength > right - left) {
         minLength = right - left;
-        sum -= arr[left++]
       }
+      sum -= arr[left++]
     }
   }
   return minLength < Infinity ? minLength : 0;
 }
+
+console.log(minSubArrayLen([2,3,1,2,4,3],7)) // 2
