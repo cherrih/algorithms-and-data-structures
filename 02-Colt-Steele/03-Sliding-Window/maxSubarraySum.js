701c6dc26c44eafd8dc3f8cbfd5e9957354efaf4
cherrih <cherri.hartigan@gmail.com>
2019-04-14 20:53:58 -0700

    Slight refactor maxSubarraySum

--- 02-Colt-Steele/03-Sliding-Window/maxSubarraySum.js
+++ 02-Colt-Steele/03-Sliding-Window/maxSubarraySum.js
@@ -3,11 +3,10 @@ const maxSubarraySum = (arr, n) => {
     return null;
   }
   let max = 0;
-  let temp = 0;
   for (let i = 0; i < n; i ++) {
     max += arr[i];
   }
-  temp = max;
+  let temp = max;
   for (let i = n; i < arr.length; i ++) {
     temp = temp + arr[i] - arr[i - n];
     max = Math.max(temp, max);
@@ -16,3 +15,18 @@ const maxSubarraySum = (arr, n) => {
 }
 
 console.log(maxSubarraySum([1,2,5,2,8,1,5], 2)) // 10
+
+// function maxSubarraySum(arr, num){
+//   if (arr.length < num) return null;
+
+//   let total = 0;
+//   for (let i=0; i<num; i++){
+//      total += arr[i];
+//   }
+//   let currentTotal = total;
+//   for (let i = num; i < arr.length; i++) {
+//      currentTotal += arr[i] - arr[i-num];
+//      total = Math.max(total, currentTotal);
+//   }
+//   return total;
+// }
