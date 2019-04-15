7a49e08e4c06b304c3de9c9b82bb86b686bd6501
cherrih <cherri.hartigan@gmail.com>
2019-04-15 15:50:04 -0700

    Implement recursive factorial solution

--- 02-Colt-Steele/05-Recursion/factorial.js
+++ 02-Colt-Steele/05-Recursion/factorial.js
@@ -1,8 +1,17 @@
+// iterative solution
+// const factorial = (num) => {
+//   let total = 1;
+//   for (let i = num; i > 1; i --) {
+//     total *= i;
+//   }
+//   return total;
+// }
+
+// recursive solution 
+
 const factorial = (num) => {
-  let total = 1;
-  for (let i = num; i > 0; i --) {
-    total *= i;
-  }
-  return total;
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
 }
-console.log(factorial(3)); // 6
+
+console.log(factorial(4)); // 6
