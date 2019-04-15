d1d5b82801521c92c62e7a63360dc61dc2b211ce
cherrih <cherri.hartigan@gmail.com>
2019-04-15 16:45:24 -0700

    Complete recursive power solution

--- 02-Colt-Steele/05-Recursion/power.js
+++ 02-Colt-Steele/05-Recursion/power.js
@@ -1,13 +1,20 @@
 // iterative
-function power(base, exp) {
-  if (exp === 0) {
-    return 1;
-  }
-  let total = 1;
-  for (let i = exp; i > 0; i --) {
-    total *= base;
-  }
-  return total;
+// function power(base, exp) {
+//   if (exp === 0) {
+//     return 1;
+//   }
+//   let total = 1;
+//   for (let i = exp; i > 0; i --) {
+//     total *= base;
+//   }
+//   return total;
+// }
+
+// recursive
+function power(base, exp){
+  if (exp === 0) return 1;
+  if (exp === 1) return base;
+  return base * power(base, exp - 1);
 }
 
 console.log(power(2,2),
