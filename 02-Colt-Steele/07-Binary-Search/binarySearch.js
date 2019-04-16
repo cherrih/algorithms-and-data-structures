b8faeca4d9b518394c1aa1e14cc08c18f2d7710f
cherrih <cherri.hartigan@gmail.com>
2019-04-15 17:57:50 -0700

    Refactor binarySearch

--- 02-Colt-Steele/07-Binary-Search/binarySearch.js
+++ 02-Colt-Steele/07-Binary-Search/binarySearch.js
@@ -2,14 +2,14 @@ function binarySearch(arr, val){
   let left = 0;
   let right = arr.length - 1;
   
-  while (left < right){
+  while (left <= right){
   let middle = Math.floor((left + right) / 2);
     if (arr[middle] === val) {
       return middle;
     } else if (arr[middle] < val) {
-      left = middle;
+      left = middle + 1;
     } else {
-      right = middle;
+      right = middle - 1;
     }
   }
   return -1;
