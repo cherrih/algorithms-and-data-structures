1f472ea2e5c16d3c0be088f94db1223ad0c721c8
cherrih <cherri.hartigan@gmail.com>
2019-04-15 17:29:12 -0700

    Complete slight refactor linear search

--- 02-Colt-Steele/06-Linear-Search/linearSearch.js
+++ 02-Colt-Steele/06-Linear-Search/linearSearch.js
@@ -1,12 +1,10 @@
 function linearSearch(arr, n) {
-  let index = -1;
   for (let i = 0; i < arr.length; i ++) {
     if (arr[i] === n) {
-      index = i;
-      break;
+      return i;
     }
   }
-  return index;
+  return -1;
 }
 
 console.log(linearSearch([1,4,5,6,2,3,9], 2))
