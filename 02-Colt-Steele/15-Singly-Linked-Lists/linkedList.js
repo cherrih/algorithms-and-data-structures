0240fba4f10660e27bca0e24ad69eaaa12021c4a
cherrih <cherri.hartigan@gmail.com>
2019-04-16 11:06:32 -0700

    Implement unshift linkedList method

--- 02-Colt-Steele/15-Singly-Linked-Lists/linkedList.js
+++ 02-Colt-Steele/15-Singly-Linked-Lists/linkedList.js
@@ -48,4 +48,16 @@ class linkedList{
     }
     return current;
   }
+  unshift(val){
+    let newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;  
+    }
+    this.length ++;
+    return this;
+  }
 }
