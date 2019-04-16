2116fba7078e3185ff3032d0927d1e007de68aa8
cherrih <cherri.hartigan@gmail.com>
2019-04-16 11:11:42 -0700

    Implement set method

--- 02-Colt-Steele/15-Singly-Linked-Lists/linkedList.js
+++ 02-Colt-Steele/15-Singly-Linked-Lists/linkedList.js
@@ -60,4 +60,20 @@ class linkedList{
     this.length ++;
     return this;
   }
+  get(i){
+    if (i < 0 || i >= this.length) return null;
+    let j = 0;
+    let curr = this.head;
+    while(j !== i){
+      curr = curr.next;
+      j++;
+    }
+    return curr;
+  }
+  set(val, i){
+    let target = this.get(i);
+    if (!target) return false;
+    target.val = val;
+    return true;
+  }
 }
