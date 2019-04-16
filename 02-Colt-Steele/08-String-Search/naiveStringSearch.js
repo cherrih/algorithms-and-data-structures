0be848c22c999985b3e85a81755a694e73f4c2cf
cherrih <cherri.hartigan@gmail.com>
2019-04-15 18:52:32 -0700

    Slight refactor naive string search

--- 02-Colt-Steele/08-String-Search/naiveStringSearch.js
+++ 02-Colt-Steele/08-String-Search/naiveStringSearch.js
@@ -2,12 +2,8 @@ function stringSearch(str, subStr) {
   let count = 0;
   for (let i = 0; i < str.length; i ++) {
     for (let j = 0; j < subStr.length; j ++) {
-      if (str[i + j] !== subStr[j]) {
-        break;
-      }
-      if (j === subStr.length - 1) {
-        count += 1;
-      }
+      if (str[i + j] !== subStr[j]) break;
+      if (j === subStr.length - 1) count += 1;
     }
   }
   return count;
