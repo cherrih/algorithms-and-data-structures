47f99de56817ea35c778484cbd62b9d46f48d3a9
cherrih <cherri.hartigan@gmail.com>
2019-04-15 18:51:45 -0700

    Implement string search naive

---
+++ 02-Colt-Steele/08-String-Search/naiveStringSearch.js
@@ -0,0 +1,17 @@
+function stringSearch(str, subStr) {
+  let count = 0;
+  for (let i = 0; i < str.length; i ++) {
+    for (let j = 0; j < subStr.length; j ++) {
+      if (str[i + j] !== subStr[j]) {
+        break;
+      }
+      if (j === subStr.length - 1) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(stringSearch('hello yellow belly', 'ell')); // 3
+console.log(stringSearch('how now brown cow', 'ow')); // 4
