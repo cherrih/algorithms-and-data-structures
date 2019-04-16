db1cbc1a794a0aa814941ee8191e45da3f617962
cherrih <cherri.hartigan@gmail.com>
2019-04-15 19:51:09 -0700

    Optimize bubbleSort

--- 02-Colt-Steele/09-Bubble-Sort/bubbleSort.js
+++ 02-Colt-Steele/09-Bubble-Sort/bubbleSort.js
@@ -1,14 +1,19 @@
 function bubbleSort(arr) {
+  let noSwaps;
   for(let i = arr.length - 1; i > 0; i --) {
+    noSwaps = true;
     for (let j = 0; j < i; j ++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j + 1];
         arr[j + 1] = arr[j]
         arr[j] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
 
 console.log(bubbleSort([4,10,9,3,2,7]));
+console.log(bubbleSort([8,1,2,3,4,5,6]));
