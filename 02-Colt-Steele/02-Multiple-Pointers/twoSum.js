2a50eed6dce851d20f8eeec79989b3b25a0191de
cherrih <cherri.hartigan@gmail.com>
2019-05-02 12:32:13 -0700

    Complete twoSum

---
+++ 02-Colt-Steele/02-Multiple-Pointers/twoSum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+  const result = [];
+  for (let i = 0; i < nums.length; i ++) {
+      for (let j = nums.length - 1; j > i; j --) {
+        const num1 = nums[i];
+        const num2 = nums[j];
+        if (num1 + num2 === target) {
+          result.push(i, j);
+          return result;
+        }
+      }
+  }
+};
