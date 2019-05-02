2eeb68f2d49b57dc310c18318fc1f73880b6390b
cherrih <cherri.hartigan@gmail.com>
2019-05-02 16:20:23 -0700

    Solve buyAndSell with linear time complexity

--- 03-Practice-Problems/Array/buyAndSell.js
+++ 03-Practice-Problems/Array/buyAndSell.js
@@ -12,3 +12,19 @@ var maxProfit = function(prices) {
 
 // time complexity: quadratic
 // space complexity: constant
+
+const maxProfitLinear = (prices) => {
+  let profit = 0;
+  let curr = prices[0];
+  for (let i = 0; i < prices.length; i ++) {
+    if (curr < prices[i] && prices[i] - curr > profit) {
+      profit = prices[i] - curr;
+    }
+    if (prices[i] < curr) {
+      curr = prices[i];
+    }
+  }
+  return profit;
+}
+
+// time complexity: linear
