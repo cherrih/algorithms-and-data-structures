0fab487eabb23989ac0772d8f3fdb0fd19a6f8ca
cherrih <cherri.hartigan@gmail.com>
2019-05-02 21:54:28 -0700

    solve productOfArray in Linear time complexity

--- 03-Practice-Problems/Array/productOfArray.js
+++ 03-Practice-Problems/Array/productOfArray.js
@@ -16,3 +16,19 @@ var productExceptSelf = function(nums) {
 
 // time complexity: quadratic
 // space complexity: linear
+
+const productExceptSelfLinear = (nums) => {
+  let result = [];
+  let left = 1;
+  let right = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = left;
+    left *= nums[i];
+  }
+  for (let j = nums.length - 1; j >= 0; j--) {
+    result[j] *= right;
+    right *= nums[j];
+  }
+  return result;
+}
+// time complexity: linear
