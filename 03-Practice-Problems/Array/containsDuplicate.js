b7c08ecbe0d709d5091f014eb020c124d61f1918
cherrih <cherri.hartigan@gmail.com>
2019-05-02 17:41:24 -0700

    Complete containsDuplicate solution

---
+++ 03-Practice-Problems/Array/containsDuplicate.js
@@ -0,0 +1,13 @@
+var containsDuplicate = function(nums) {
+  const map = {};
+  for (let i = 0; i < nums.length; i ++) {
+      if (map[nums[i]] === 1) {
+          return true;
+      } else {
+          map[nums[i]] = 1;
+      }
+  }
+  return false;
+};
+
+console.log(containsDuplicate([0,1,2,0,8]));
