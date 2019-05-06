fec2383315880815ad44c8f988ec71e94d0c1cb6
cherrih <cherri.hartigan@gmail.com>
2019-05-06 11:03:55 -0700

    Add time/space complexity

--- 03-Practice-Problems/String/isHappy.js
+++ 03-Practice-Problems/String/isHappy.js
@@ -3,14 +3,19 @@
 var isHappy = function(n) {
   const seen = {};
   while (n !== 1 && !seen[n]) {
-      seen[n] = true;
-      n = sumOfSquares(n);
+    seen[n] = true;
+    n = sumOfSquares(n);
   }
   return n === 1 ? true : false;
-};
+}
 
 var sumOfSquares = function(num) {
   return num.toString().split('').reduce((sum, num) => {
-      return sum += num * num;
-  }, 0)
-};
+    return sum += num * num;
+  }, 0);
+}
+
+console.log(isHappy(19)); // true
+console.log(isHappy(2)); // false
+
+// time complexity: logarithmic, space complexity: constant
