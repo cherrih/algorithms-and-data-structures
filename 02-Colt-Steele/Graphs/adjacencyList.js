029086ede3acef9f0b0deb85fdaee8d6bcc3a60c
cherrih <cherri.hartigan@gmail.com>
2019-05-06 20:29:20 -0700

    Add add edge method

--- 02-Colt-Steele/Graphs/adjacencyList.js
+++ 02-Colt-Steele/Graphs/adjacencyList.js
@@ -6,9 +6,15 @@ class Graph {
   addVertex(name){
     this.adjacencyList[name] = [];
   }
+  addEdge(v1, v2){
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 }
 
 var g = new Graph;
 g.addVertex('Tokyo');
 g.addVertex('San Francisco');
+console.log(g);
+g.addEdge('Tokyo', 'San Francisco');
 console.log(g);
